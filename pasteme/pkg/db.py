ff38679440e6579c65074748f7932f8ba86fef2d
lim-mil <limyel@outlook.com>
2021-03-27 14:07:48 +0800

    fix media目录不存在

--- pasteme/pkg/db.py
+++ pasteme/pkg/db.py
@@ -1,3 +1,5 @@
+import os
+
 from pasteme import config
 from playhouse.db_url import connect
 
@@ -10,3 +12,6 @@ def create_tables():
     from pasteme.models.UserModel import UserModel
 
     MYSQL_DB.create_tables([UserFileModel, UserModel], safe=True)
+
+    if not os.path.exists(os.path.join(config.BASE_DIR, 'media')):
+        os.mkdir(os.path.join(config.BASE_DIR, 'media'))
